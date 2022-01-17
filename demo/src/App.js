6d87b6d50de2c4ccff4af94e3618c81b6d779d6e
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-01-17 11:29:47 +0200

    Updated on Linux - Mon Jan 17 11:29:47 EET 2022

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,49 +1,47 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-export default function App() {
-  const [rssUrl, setRssUrl] = useState("");
-  const [items, setItems] = useState([]);
 
-  /* ============================================= */
-  const getRss = async (e) => {
-    e.preventDefault();
-    const urlRegex = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
-    if (!urlRegex.test(rssUrl)) {
-      return;
-    }
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+
+  async componentDidMount() {
+
+    const rssUrl = "https://www.leelasrecipes.com/index.xml" ;
+ 
     const res = await fetch(`https://api.allorigins.win/get?url=${rssUrl}`);
     const { contents } = await res.json();
     const feed = new window.DOMParser().parseFromString(contents, "text/xml");
     const items = feed.querySelectorAll("item");
-    const feedItems = [...items].map((el) => ({
-      link: el.querySelector("link").innerHTML,
-      // author: el.querySelector("author").innerHTML,
-      pubDate: el.querySelector("pubDate").innerHTML,
-      title: el.querySelector("title").innerHTML
-    }));
-    setItems(feedItems);
-  };
+    
+    this.setState({ data: items });
+  }
 
   /* ============================================= */
+
+ 
+  /* ============================================= */
+  render() {
+
   return (
     <div className="App">
-      <form onSubmit={getRss}>
-        <div>
-          <label> rss url</label>
-          <br />
-          <input onChange={(e) => setRssUrl(e.target.value)} value={rssUrl} />
-        </div>
-        <input type="submit" />
-      </form>
-      {items.map((item) => {
-        return (
+      {""}
+
+      {this.state.data.map((item) => {
           <div key={item.link}>
             <h3><a href={item.link}>{item.title}</a></h3>
             <p>{item.pubDate}</p>
             <hr />           
           </div>
-        );
       })}
+
     </div>
   );
 }
+
+}
+
+export default App ;
