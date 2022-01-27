3260c3aaf3d4723a9dedb7d8cfaaa597e895b24f
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-01-27 20:30:39 +0200

    toggle swich added

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const slicedArray = finalData.slice(0, 20);
   console.log(Data.length) ; 
   return <div className="container">
-    <h1>MGGK IMAGE FINDER</h1>
+    <h1>MGGK IMAGE FINDER APP</h1>
     <div className="row"><div className="col-12">
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">Search here (anchor text or url)</label>
@@ -20,6 +20,7 @@ const App = () => {
     <div className="row">
       <div className="col-12">
         <h4>Results found = {finalData.length}</h4>
+        {displayCollapsedDetails()}
       </div>
       {slicedArray}
     </div>
@@ -27,6 +28,13 @@ const App = () => {
 };
 
 export default App;
+
+/* =============================================== */
+function displayCollapsedDetails () {
+  return  <div>
+            <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Dispaly all image URLs (toggle)</a>
+          </div>
+} ;
 
 function calcFunction (query) {  
   const tmp1 = Data.filter(post => {
@@ -41,9 +49,14 @@ function calcFunction (query) {
   const tmp2 = tmp1.map((post,index) => {
         return <div className="col-sm-6 col-md-4 col-lg-2 col-xl-2" key={index}>
             <hr />
-            <p><a href={post.url}> <img src={post.url} width='200px' /> </a></p>
-            <textarea>{post.url}</textarea>
+            <p><a href={post.url}> <img src={post.url} width='100%;' /> </a></p>
+            <div class="collapse" id="collapseExample">
+              <div class="card card-body">
+                {post.url}
+              </div>
+            </div>
         </div>
   }) ;
   return tmp2 ; 
 } ; 
+
