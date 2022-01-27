f319bd523e93ea73a2c6b48c4e82638d972e32cb
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-01-27 21:28:46 +0200

    Update App.js

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -3,41 +3,22 @@ import Data from './data/data_out/mggk_summary_cloudflare_ImagesUrlsWPcontentUpl
 import {useState} from 'react';
 import './App.css' ;
 
-const App = () => {
-  const [query, setQuery] = useState('') ;
-  // Getting array elements and slicing it to only show N elements
-  const finalData = calcFunction(query) ; 
-  const slicedArray = finalData.slice(0, 20);
-  console.log(Data.length) ; 
-  return <div className="container">
-    <h1>MGGK IMAGE FINDER APP</h1>
-    <div className="row"><div className="col-12">
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">Search here (anchor text or url)</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="search here" onChange={event => setQuery(event.target.value)} />
-      </div>
-    </div></div>
-    <div className="row">
-      <div className="col-12">
-        <h4>Results found = {finalData.length}</h4>
-        {displayCollapsedDetails()}
-      </div>
-      {slicedArray}
-    </div>
-  </div>;
-};
-
-export default App;
-
 /* =============================================== */
+function calculate350pxImageUrl(url) {
+    let imageBaseName = url.substring(url.lastIndexOf('/') + 1);
+    let prefixAdd = 'https://cdn.mygingergarlickitchen.com/images/350px/350px-' ;
+    let finalImage = prefixAdd + imageBaseName ; 
+    return finalImage ; 
+}
+
 function displayCollapsedDetails () {
   return  <div>
-            <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Dispaly all image URLs (toggle)</a>
+            <a className="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Dispaly all image URLs (toggle)</a>
           </div>
 } ;
 
-function calcFunction (query) {  
-  const tmp1 = Data.filter(post => {
+function getSearchResults (query) {  
+  const filteredData = Data.filter(post => {
   if (query === '') { return post; } 
   else if (post.url.toLowerCase().includes(query.toLowerCase())) {
     console.log(query);
@@ -46,17 +27,47 @@ function calcFunction (query) {
   else { return null ; }
   }) ;      
   
-  const tmp2 = tmp1.map((post,index) => {
+  const printFilteredDataItems = filteredData.map((post,index) => {
+        const image350px = calculate350pxImageUrl(post.url) ;  
         return <div className="col-sm-6 col-md-4 col-lg-2 col-xl-2" key={index}>
             <hr />
-            <p><a href={post.url}> <img src={post.url} width='100%;' /> </a></p>
-            <div class="collapse" id="collapseExample">
-              <div class="card card-body">
-                {post.url}
+            <p><a href={post.url}> <img src={image350px} width='100%;' /> </a></p>
+            <div className="collapse" id="collapseExample">
+              <div className="card card-body">
+                <a href={post.url}><small>{post.url}</small></a>
+                <br />
+                <small><strong>Displayed image:</strong> {image350px}</small>
               </div>
             </div>
         </div>
   }) ;
-  return tmp2 ; 
+  return printFilteredDataItems ; 
 } ; 
+/* =============================================== */
 
+const App = () => {
+  const [query, setQuery] = useState('') ;
+  // Getting array elements and slicing it to only show N elements
+  const finalData = getSearchResults(query) ; 
+  const numImagesToShow = 50 ; 
+  const slicedArray = finalData.slice(0, numImagesToShow);
+  console.log(Data.length) ; 
+  return <div className="container">
+    <h1>MGGK IMAGE FINDER (APP)</h1>
+    <div className="row"><div className="col-12">
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">Search here (anchor text or url)</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="search here" onChange={event => setQuery(event.target.value)} />
+      </div>
+    </div></div>
+    <div className="row">
+      <div className="col-12">
+        <h3>{finalData.length} images found (showing max {numImagesToShow})</h3> 
+        {displayCollapsedDetails()}
+      </div>
+      {slicedArray}
+    </div>
+  </div>;
+};
+
+export default App;
