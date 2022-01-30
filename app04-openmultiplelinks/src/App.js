c415341e7b1f1177c664c57737a382c11a438ab0
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-01-30 17:43:11 +0200

    project files updated

--- app04-openmultiplelinks/src/App.js
+++ app04-openmultiplelinks/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function openTheseLinks0() {
+  window.open('https://www.hindustantimes.com/','_blank') ;
+  window.open('https://timesofindia.indiatimes.com/','_blank') ;
+  window.open('https://www.bbc.com/','_blank') ;
+  window.open('https://yle.fi/news','_blank') ;
+  window.open('https://slashdot.org/','_blank') ;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="display-5">App - Open Multiple Websites (in tabs)</h1>
+      <hr />
+      <button type="button" className="btn btn-primary" onClick={openTheseLinks0}>Open All News Websites</button>
     </div>
   );
 }
