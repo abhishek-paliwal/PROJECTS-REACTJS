12a4d34bd3fff28cdbd4acf4392091a5f3195fbc
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-01-30 18:18:15 +0200

    Update App.js

--- app04-openmultiplelinks/src/App.js
+++ app04-openmultiplelinks/src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
+/* ========================== */
 
-function openTheseLinks0() {
+function openLinks1() {
+  window.open('https://slashdot.org/','_blank') ;
   window.open('https://www.hindustantimes.com/','_blank') ;
   window.open('https://timesofindia.indiatimes.com/','_blank') ;
   window.open('https://www.bbc.com/','_blank') ;
+  window.open('https://www.nytimes.com/','_blank') ;
   window.open('https://yle.fi/news','_blank') ;
-  window.open('https://slashdot.org/','_blank') ;
 }
 
+function displayLinks1() {
+  return <div className="card">
+    <div className="card-body">
+      <h5 className="card-title">News Websites</h5>
+        <button type="button" className="btn btn-primary" onClick={openLinks1}>Open All News Websites</button>
+    </div>
+  </div>
+}
+
+/* ========================== */
 function App() {
+  const displayLinks1results = displayLinks1() ;
   return (
-    <div className="container">
-      <h1 className="display-5">App - Open Multiple Websites (in tabs)</h1>
-      <hr />
-      <button type="button" className="btn btn-primary" onClick={openTheseLinks0}>Open All News Websites</button>
+      <div className="section">
+        <div className="row m-2">
+          <h1 className="display-5">Open Multiple Websites (in tabs) - App</h1>
+          <hr />
+          {displayLinks1results}
+        </div>
     </div>
   );
 }
