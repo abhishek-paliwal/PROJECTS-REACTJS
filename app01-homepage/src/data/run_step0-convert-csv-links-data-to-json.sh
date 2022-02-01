46b4191138c38cc31fa8ebfeedab10cd06513d44
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-02-01 02:26:56 +0200

    Update run_step0-convert-csv-links-data-to-json.sh

--- app01-homepage/src/data/run_step0-convert-csv-links-data-to-json.sh
+++ app01-homepage/src/data/run_step0-convert-csv-links-data-to-json.sh
@@ -2,6 +2,7 @@
 ################################################################################
 ## set variables
 PROJECT_NAME="app01-homepage" ;
+PROJECT_APP_URL="https://app01-homepage.abhishekpali.us/index.html" ;
 ##
 PROJECT_DATA_DIR="$REPO_REACTJS/$PROJECT_NAME/src/data" ;
 PROJECT_DATA_DIR_IN="$REPO_REACTJS/$PROJECT_NAME/src/data/data_in" ;
@@ -10,7 +11,7 @@ mkdir -p "$PROJECT_DATA_DIR_IN" "$PROJECT_DATA_DIR_OUT" ;
 ##
 CSVFILE_BASENAME="combinedCategoryLinksData.csv" ;
 CSVFILE_OUT="$PROJECT_DATA_DIR_OUT/$CSVFILE_BASENAME" ;
-JSON_FILE_OUT="$PROJECT_DATA_DIR_OUT/$(basename $CSVFILE_BASENAME).json" ;
+JSON_FILE_OUT="$PROJECT_DATA_DIR_OUT/$( basename $CSVFILE_BASENAME | sed 's|.csv|.json|ig' )" ;
 ################################################################################
 
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -20,6 +21,7 @@ TMP_HEADERFILE="$DIR_Y/tmp_csv_header.txt" ;
 cat $(fd -I -t f -e csv --search-path="$PROJECT_DATA_DIR_IN" | head -1) > $TMP_HEADERFILE ; 
 cat $TMP_HEADERFILE > $CSVFILE_OUT
 cat $(fd -I -t f -e csv --search-path="$PROJECT_DATA_DIR_IN") | grep -iv 'ANCHORTEXT' | grep -iv '^$' | sort -u >> $CSVFILE_OUT ;
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 ## convert downloaded data to json format
 function step2_convertCsvFileToJson () {
@@ -28,13 +30,14 @@ function step2_convertCsvFileToJson () {
     cat $inFile | grep -iv 'ANCHORTEXT' > $tmpFile ;
     echo "[" ;
     while read line; do
-        ## SEPARATE FIELDS
-        F_CATEGORY="$(echo $line | cut -d',' -f1)" ;
-        F_ANCHORTEXT="$(echo $line | cut -d',' -f2)" ;
-        F_URL="$(echo $line | cut -d',' -f3)" ;
+        ## IDENTIFYING COMMA-SEPARATED FIELDS
+        #echo "LINE = > $line" ;
+        F_CATEGORY="$(echo $line | cut -d ',' -f1)" ;
+        F_ANCHORTEXT="$(echo $line | cut -d ',' -f2)" ;
+        F_URL="$(echo $line | cut -d ',' -f3)" ;
         echo "{ \"CATEGORY\" : \"$F_CATEGORY\",\"ANCHORTEXT\" : \"$F_ANCHORTEXT\",\"URL\" : \"$F_URL\" },"
     done < $tmpFile ;
-    echo "{ \"CATEGORY\" : \"PERSONAL\",\"ANCHORTEXT\" : \"This Homepage\",\"URL\" : \"#\" },"
+    echo "{ \"CATEGORY\" : \"PERSONAL\",\"ANCHORTEXT\" : \"Our Homepage\",\"URL\" : \"$PROJECT_APP_URL\" }" ;
     echo "]" ;
 }
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -46,7 +49,7 @@ step2_convertCsvFileToJson | jq > $JSON_FILE_OUT ;
 echo; echo "################################################################################" ;
 echo ">> WORDCOUNT IN ORIGINAL FILE: $(wc $CSVFILE_OUT)" ;
 echo; echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
-echo ">> PRINT FIRST FEW LINES FROM JSON FILE => $JSON_FILE_OUT" ;
-head $JSON_FILE_OUT ;
+echo ">> PRINTING FULL JSON FILE => $JSON_FILE_OUT" ;
+cat $JSON_FILE_OUT ;
 echo "################################################################################" ;
 ################################################################################
