0edf1e893e42e26a25c2321937ed7d21f5302c4b
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-02-07 22:01:22 +0200

    8x updated

--- app02-foodblogfeeds/src/index.js
+++ app02-foodblogfeeds/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyApp from './MyApp';
-import MyNewsFeeds from './MyNewsFeeds';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+//import MyApp from './MyApp';
+//import MyNewsFeeds from './MyNewsFeeds';
 
+import FeedFoodBlogs from './pages/FeedFoodBlogs';
+import FeedWorldNews from './pages/FeedWorldNews';
+import FeedTechnologyNews from './pages/FeedTechnologyNews';
+
+//
 ReactDOM.render(
   <React.StrictMode>
-    <MyApp />
-    <MyNewsFeeds />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavBar />}>
+            <Route index element={<FeedFoodBlogs />} />
+            <Route path="feedfoodblogs" element={<FeedFoodBlogs />} />
+            <Route path="feedworldnews" element={<FeedWorldNews />} />
+            <Route path="feedtechnologynews" element={<FeedTechnologyNews />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
