ddfb4b57fc2ac070742b52e7774d5f5695975c96
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-02-07 16:11:42 +0200

    More links added + slider value fixed

--- app01-homepage/src/components/NavBar.js
+++ app01-homepage/src/components/NavBar.js
@@ -13,24 +13,43 @@ const NavBar = () => {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" target="_blank" href="https://www.mygingergarlickitchen.com/" id="navbarDropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            MGGK
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown1">
+            <li><a className="dropdown-item" target="_blank" href="https://www.mygingergarlickitchen.com/">MGGK - Homepage</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" target="_blank" href="https://www.gmail.com">MGGK Gmail</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://www.facebook.com/MyGingerGarlicKitchen">MGGK Facebook</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://instagram.com/mygingergarlickitchen/">MGGK Instragram</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://www.pinterest.com/anupamapaliwal">MGGK Pinterest</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://www.youtube.com/c/Mygingergarlickitchen">MGGK Youtube</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" target="_blank" href="https://app.grammarly.com/">Grammarly</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://www.verywellfit.com/recipe-nutrition-analyzer-4157076">Recipe Nutrition Analyzer</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://websiteseochecker.com/domain-authority-checker/">DA Checker (Domain authority)</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://www.whois.com/whois">Whois Checker</a></li>
+          </ul>
+        </li>
+
+
         <li className="nav-item">
-          <a className="nav-link" href="https://www.mygingergarlickitchen.com/">MGGK</a>
+          <a className="nav-link" target="_blank" href="https://www.concepro.com/">Concepro</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="https://www.concepro.com/">Concepro</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="https://www.leelasrecipes.com/">LeelasRecipes</a>
+          <a className="nav-link" target="_blank" href="https://www.leelasrecipes.com/">LeelasRecipes</a>
         </li>        
         <li className="nav-item">
-          <a className="nav-link" href="https://adoria.xyz/">Ado.xyz</a>
+          <a className="nav-link" target="_blank" href="https://adoria.xyz/">Ado.xyz</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="https://downloads.concepro.com/dropbox-public-files/LCE/big_bold_random_quotes.html">Want-Wisdom?</a>
+          <a className="nav-link" target="_blank" href="https://downloads.concepro.com/dropbox-public-files/LCE/big_bold_random_quotes.html">Want-Wisdom?</a>
         </li>
 
         <li className="nav-item">
-          <a className="nav-link" href="https://downloads.concepro.com/dropbox-public-files/LCE/personal_shorturl_creator.html">URL-Shorteners</a>
+          <a className="nav-link" target="_blank" href="https://downloads.concepro.com/dropbox-public-files/LCE/personal_shorturl_creator.html">URL-Shorteners</a>
         </li>
 
         <li className="nav-item dropdown">
@@ -38,15 +57,12 @@ const NavBar = () => {
             Designing-Tools
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown1">
-            <li><a className="dropdown-item" href="https://abhishek-paliwal.github.io/wallpapers-index.html">Design4Love</a></li>
-            <li><a className="dropdown-item" href="https://downloads.concepro.com/dropbox-public-files/all-polygonal-backgrounds/all-lowpoly-backgrounds-index.html">Poly-Textures</a></li>
-            <li><a className="dropdown-item" href="https://downloads.concepro.com/dropbox-public-files/LCE/google_material_design_colors.html">Material-Colors</a></li>
-            <li><a className="dropdown-item" href="https://downloads.concepro.com/dropbox-public-files/logos/all-logos-index.html">All-Logos</a></li>
-            <li><a className="dropdown-item" href="https://downloads.concepro.com/dropbox-public-files/LCE/_typography_wallpaper_creator/_CSS-Typography-EFFECTS/slabText-master/index_final.html">Bold-Wallpapers</a></li>
-            <li><a className="dropdown-item" href="https://abhishek-paliwal.github.io/wallpaper_creators/JSON-templates-for-design/Index-of-all-JSON-files.html">All JSON Templates</a></li>
-
-            
-
+            <li><a className="dropdown-item" target="_blank" href="https://abhishek-paliwal.github.io/wallpapers-index.html">Design4Love</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://downloads.concepro.com/dropbox-public-files/all-polygonal-backgrounds/all-lowpoly-backgrounds-index.html">Poly-Textures</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://downloads.concepro.com/dropbox-public-files/LCE/google_material_design_colors.html">Material-Colors</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://downloads.concepro.com/dropbox-public-files/logos/all-logos-index.html">All-Logos</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://downloads.concepro.com/dropbox-public-files/LCE/_typography_wallpaper_creator/_CSS-Typography-EFFECTS/slabText-master/index_final.html">Bold-Wallpapers</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://abhishek-paliwal.github.io/wallpaper_creators/JSON-templates-for-design/Index-of-all-JSON-files.html">All JSON Templates</a></li>
           </ul>
         </li>
 
@@ -55,10 +71,10 @@ const NavBar = () => {
             Apps
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown2">
-            <li><a className="dropdown-item" href="https://app-mggk01-image-finder.abhishekpali.us/index.html">app-mggk01-image-finder</a></li>
-            <li><a className="dropdown-item" href="https://app02-foodblogfeeds.abhishekpali.us/index.html">app02-foodblogfeeds</a></li>
-            <li><a className="dropdown-item" href="https://app03-tictactoe.abhishekpali.us/index.html">app03-tictactoe</a></li>
-            <li><a className="dropdown-item" href="https://app04-openmultiplelinks.abhishekpali.us/index.html">app04-openmultiplelinks</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://app-mggk01-image-finder.abhishekpali.us/index.html">app-mggk01-image-finder</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://app02-foodblogfeeds.abhishekpali.us/index.html">app02-foodblogfeeds</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://app03-tictactoe.abhishekpali.us/index.html">app03-tictactoe</a></li>
+            <li><a className="dropdown-item" target="_blank" href="https://app04-openmultiplelinks.abhishekpali.us/index.html">app04-openmultiplelinks</a></li>
           </ul>
         </li>
       </ul>
--- app01-homepage/src/components/SearchBarAndResults.js
+++ app01-homepage/src/components/SearchBarAndResults.js
@@ -3,21 +3,26 @@ import Data from '../data/data_out/combinedCategoryLinksData.json' ;
 import {useState} from 'react';
 
 /* =============================================== */
-function showInputField (triggerSearchAtChars, setQuery, textareaRef, cursorPosition) {
+function showInputField (triggerSearchAtChars, setQuery, setNumImagesToShow) {
   return <div className="m-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">Enter single word (url/anchor-text, {triggerSearchAtChars} chars min)</label>
         <input type="text" 
         className="form-control" 
         id="exampleFormControlInput1" 
         placeholder="search here" 
-        onChange={event => {(event.target.value.length >= triggerSearchAtChars) ? setQuery(event.target.value) : setQuery('') }} />
+        onChange={event => {(event.target.value.length >= triggerSearchAtChars) ? setQuery(event.target.value) : setQuery('') ; setNumImagesToShow(4) }} />
       </div>
 }
 
 function showRangeSlider (numImagesToShow, setNumImagesToShow) {
   return <div className="m-3">
         <label htmlFor="customRange1" className="form-label">Number of results to display</label>
-        <input type="range" className="form-range" min="1" max="20" id="customRange1" onChange={e => setNumImagesToShow(e.target.value)} />
+        <input type="range" 
+        className="form-range" 
+        min="1" 
+        max="20" 
+        id="customRange1" 
+        onChange={e => setNumImagesToShow(e.target.value) } />
       </div>
 }
 
@@ -44,16 +49,16 @@ function getSearchResults (query) {
 
 const SearchBarAndResults = () => {
   const [query, setQuery] = useState('') ;
-  const [numImagesToShow, setNumImagesToShow] = useState(4) ;
+  const [numImagesToShow, setNumImagesToShow] = useState(0) ;
   // Getting array elements and slicing it to only show N elements
   const finalData = getSearchResults(query) ; 
   const slicedArray = finalData.slice(0, numImagesToShow);
-  const triggerSearchAtChars = 3 ; 
-  console.log(Data.length) ; 
+  const triggerSearchAtChars = 2 ; 
+  // console.log(Data.length) ; 
   /* ================ */
   return <div className="container">
     <div className="row bg-dark text-white"><div className="col-12">
-      {showInputField (triggerSearchAtChars, setQuery)}
+      {showInputField (triggerSearchAtChars, setQuery, setNumImagesToShow)}
       {showRangeSlider (numImagesToShow, setNumImagesToShow)}
     </div></div>
     <div className="row">
