c094e2413a22d102b15eb426908715c4291a6019
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-02-11 16:41:28 +0200

    Update UnitsCalculator.js

--- app-mggk01-image-finder/src/pages/UnitsCalculator.js
+++ app-mggk01-image-finder/src/pages/UnitsCalculator.js
@@ -56,8 +56,83 @@ function CalculateOvenTimings (unit1value,unit2value,unit1valueNew) {
         </div>
   </div>
 }
-
-
+////
+function convertWeightUnits (unit1value) {
+  // Get the following ratios from Google
+  // Ratios below
+  const tsp2tbspRatio =  1/3 ;
+  const oz2gramsRatio = 28.35 ; 
+  const pounds2gramsRatio = 454 ; 
+  // Calculations below
+  const tsp2tbsp =  unit1value * tsp2tbspRatio ;
+  const tbsp2tsp =  unit1value / tsp2tbspRatio ;
+  //
+  const oz2grams =  unit1value * oz2gramsRatio ;
+  const grams2oz =  unit1value / oz2gramsRatio ;
+  //
+  const pounds2grams =  unit1value * pounds2gramsRatio ;
+  const grams2pounds =  unit1value / pounds2gramsRatio ;
+  //
+  return <div className="p-2 border border-secondary rounded"> 
+          <p>Cooking units conversions - Weight (automatic)</p>        
+            <table className="table table-striped">
+              <thead>
+                <tr><th scope="col">Unit1</th><th scope="col">Unit2</th></tr>
+              </thead>
+              <tbody>
+                <tr><td>{unit1value} tsp =</td><td>{tsp2tbsp} tbsp</td></tr>
+                <tr><td>{unit1value} tsp =</td><td> {tsp2tbsp} tbsp</td></tr>
+                <tr><td>{unit1value} tbsp =</td><td> {tbsp2tsp} tbsp</td></tr>
+                <tr><td>{unit1value} Oz =</td><td> {oz2grams} grams</td></tr>
+                <tr><td>{unit1value} grams =</td><td> {grams2oz} Oz</td></tr>
+                <tr><td>{unit1value} pounds (lb) =</td><td> {pounds2grams} grams</td></tr>
+                <tr><td>{unit1value} grams =</td><td> {grams2pounds} pounds (lb)</td></tr>
+            </tbody>
+            </table>
+        </div>
+}
+////
+function convertVolumeUnits (unit1value) {
+  // Get the following ratios from Google
+  // Ratios below
+  const ml2cupsRatio = 1/237 ; 
+  const ml2tbspRatio = 1/14.78 ;
+  const fluidOz2mlRatio = 29.574 ; 
+  const fluidOz2cupsRatio = 1/8 ; 
+  //
+  // Calculations below
+  const ml2cups =  unit1value * ml2cupsRatio ;
+  const cups2ml =  unit1value / ml2cupsRatio ;
+  //
+  const ml2tbsp =  unit1value * ml2tbspRatio ;
+  const tbsp2ml =  unit1value / ml2tbspRatio ;
+  //
+  const fluidOz2ml =  unit1value * fluidOz2mlRatio ;
+  const ml2fluidOz =  unit1value / fluidOz2mlRatio ;
+  //
+  const fluidOz2cups =  unit1value * fluidOz2cupsRatio ;
+  const cups2fluidOz =  unit1value / fluidOz2cupsRatio ;
+  //
+  //
+  return <div className="p-2 border border-secondary rounded"> 
+          <p>Cooking units conversions - Volume (automatic)</p>        
+            <table className="table table-striped">
+              <thead>
+                <tr><th scope="col">Unit1</th><th scope="col">Unit2</th></tr>
+              </thead>
+              <tbody>     
+        <tr><td>{unit1value} ml =</td><td>{ml2cups} cups</td></tr>
+        <tr><td>{unit1value} cups =</td><td>{cups2ml} ml</td></tr>
+        <tr><td>{unit1value} ml =</td><td>{ml2tbsp} tbsp</td></tr>
+        <tr><td>{unit1value} tbsp =</td><td>{tbsp2ml} ml</td></tr>
+        <tr><td>{unit1value} fluid Oz =</td><td>{fluidOz2ml} ml</td></tr>
+        <tr><td>{unit1value} ml =</td><td>{ml2fluidOz} fluid Oz</td></tr>
+        <tr><td>{unit1value} fluid Oz =</td><td>{fluidOz2cups} cups</td></tr>
+        <tr><td>{unit1value} cups =</td><td>{cups2fluidOz} fluid Oz</td></tr>
+            </tbody>
+            </table>
+        </div>
+}
 /* =============================================== */
 
 const UnitsCalculator = () => {
@@ -68,15 +143,21 @@ const UnitsCalculator = () => {
   const showCalculatedInputs = showInputFields(unit1value,unit2value,unit1valueNew,setUnit1value,setUnit2value,setUnit1valueNew) ; 
   //
   const convertTemperatures = CalculateCelciusToFahrenheit (unit1value) ; 
-  //
   const convertOvenTimings = CalculateOvenTimings(unit1value,unit2value,unit1valueNew,setUnit1value,setUnit2value,setUnit1valueNew) ;  
   // 
+  const convertWeightUnitsValue = convertWeightUnits(unit1value) ;  
+  const convertVolumeUnitsValue = convertVolumeUnits(unit1value) ;  
+  //
   return <div className="container">
     <h1 className="display-6">Calculator = Convert unit1 to unit2</h1>
     <div className="section mb-2">{showCalculatedInputs}</div>
     <div className="section mb-2"><div className="row">
       <div className="col-lg-6 col-sm-12">{convertTemperatures}</div>
       <div className="col-lg-6 col-sm-12">{convertOvenTimings}</div>
+    </div></div>
+    <div className="section mb-2"><div className="row">
+      <div className="col-lg-6 col-sm-12">{convertWeightUnitsValue}</div>
+      <div className="col-lg-6 col-sm-12">{convertVolumeUnitsValue}</div>
     </div></div>
   </div>;
 };
