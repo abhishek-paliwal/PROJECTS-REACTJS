75204626a3fa2f873f6b41fff7f34ec523f84d2e
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-02-28 05:29:26 +0200

    Update SearchBarAndResults.js

--- app01-homepage/src/components/SearchBarAndResults.js
+++ app01-homepage/src/components/SearchBarAndResults.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Data from '../data/data_out/combinedCategoryLinksData.json' ;
+import DataLinks from '../data/data_out/combinedCategoryLinksData.json' ;
+import DataMGGK from '../data/data_out/mggkWebpagesData.json' ;
 import {useState} from 'react';
 
 /* =============================================== */
@@ -13,7 +14,17 @@ function showInputField (triggerSearchAtChars, setQuery, setNumImagesToShow) {
         onChange={event => {(event.target.value.length >= triggerSearchAtChars) ? setQuery(event.target.value) : setQuery('') ; setNumImagesToShow(4) }} />
       </div>
 }
-
+//////
+function showDropdownSelectMenu (setInputDataToUse) {
+  return <div className="m-3">
+        <select multiple={false} className="form-select" aria-label="Default select menu" value="defaultOption" onChange={e => setInputDataToUse(e.target.value) }>
+        <option value="defaultOption">Select from option below</option>
+        <option value="dataLinks">Links (default)</option>
+        <option value="dataMGGK">MGGK Webpages</option>
+        </select>
+      </div>
+}
+//////
 function showRangeSlider (numImagesToShow, setNumImagesToShow) {
   return <div className="m-3">
         <label htmlFor="customRange1" className="form-label">Number of results to display</label>
@@ -25,9 +36,11 @@ function showRangeSlider (numImagesToShow, setNumImagesToShow) {
         onChange={e => setNumImagesToShow(e.target.value) } />
       </div>
 }
-
-function getSearchResults (query) {  
-  const filteredData = Data.filter(post => {
+//////
+function getSearchResults_for_datalinks (query) { 
+  let inputData = DataLinks ;
+  //
+  const filteredData = inputData.filter(post => {
   if (query === '') { return post; } 
   else if (post.URL.toLowerCase().includes(query.toLowerCase()) || post.ANCHORTEXT.toLowerCase().includes(query.toLowerCase()) ) {
     console.log(query);
@@ -35,23 +48,63 @@ function getSearchResults (query) {
   }
   else { return null ; }
   }) ;      
-
+  //
   const printFilteredDataItems = filteredData.map((post,index) => {
         return  <div className="col-sm-6 col-md-4 col-lg-3 col-xl-3" key={index}>
-                  <div className=" m-2 p-2 rounded bg-warning rounded text-center">
+                  <div className="m-2 p-2 rounded bg-warning rounded text-center">
                     <a target="_blank" rel="noopener noreferrer" href={post.URL}><strong>{post.ANCHORTEXT}</strong></a>
                   </div>
                 </div>
   }) ;
   return printFilteredDataItems ; 
 } ; 
+//////
+function getSearchResults_for_dataMGGK (query) { 
+  const inputData = DataMGGK ;
+  const filteredData = inputData.filter(post => {
+  if (query === '') { return post; } 
+  else if (post.url_value.toLowerCase().includes(query.toLowerCase()) || post.seo_title_value.toLowerCase().includes(query.toLowerCase()) ) {
+    console.log(query);
+    return post; 
+  }
+  else { return null ; }
+  }) ;      
+  //
+  const printFilteredDataItems = filteredData.map((post,index) => {
+        return  <div className="col-sm-6 col-md-4 col-lg-3 col-xl-3" key={index}>
+                  <div className="m-2 p-2 rounded bg-warning rounded text-center">
+                    <a target="_blank" rel="noopener noreferrer" href={post.url_value}><strong>{post.seo_title_value}</strong></a>
+                  </div>
+                </div>
+  }) ;
+  return printFilteredDataItems ; 
+} ; 
+//////
+function whichDatatoUseFinally (query,inputDataToUse) {
+  let finalData = getSearchResults_for_datalinks(query) ; 
+  //
+  if (inputDataToUse === "dataLinks" ) { 
+    finalData = getSearchResults_for_datalinks(query) ; 
+  } 
+  else if (inputDataToUse === "dataMGGK") { 
+    finalData = getSearchResults_for_dataMGGK(query) ;
+  }
+  else { 
+    finalData = getSearchResults_for_datalinks(query) ;
+  } ; 
+  console.log(inputDataToUse) ;
+  return finalData ; 
+}
+//////
 /* =============================================== */
 
 const SearchBarAndResults = () => {
   const [query, setQuery] = useState('') ;
   const [numImagesToShow, setNumImagesToShow] = useState(0) ;
+  const [inputDataToUse, setInputDataToUse] = useState('dataLinks') ;
+  //
   // Getting array elements and slicing it to only show N elements
-  const finalData = getSearchResults(query) ; 
+  const finalData = whichDatatoUseFinally (query,inputDataToUse) ;
   const slicedArray = finalData.slice(0, numImagesToShow);
   const triggerSearchAtChars = 2 ; 
   // console.log(Data.length) ; 
@@ -59,11 +112,12 @@ const SearchBarAndResults = () => {
   return <div className="container">
     <div className="row bg-dark text-white"><div className="col-12">
       {showInputField (triggerSearchAtChars, setQuery, setNumImagesToShow)}
+      {showDropdownSelectMenu (setInputDataToUse)}
       {showRangeSlider (numImagesToShow, setNumImagesToShow)}
     </div></div>
     <div className="row">
       <div className="col-12 m-2 p-2 text-white">
-        <h4><strong>{finalData.length} links found</strong> { (query.length >= triggerSearchAtChars) ? 'for term ' + query : 'in database' } (showing max {numImagesToShow})</h4> 
+        <h4><strong>{finalData.length} links found</strong> { (query.length >= triggerSearchAtChars) ? 'for term ' + query : 'in this database > ' + inputDataToUse } (showing max {numImagesToShow})</h4> 
       </div>
       {slicedArray}
     </div>
